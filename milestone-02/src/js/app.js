7f7e64e23aa95516ea4526374c716995c7d58fc4
miles20swank <miles20swank@gmail.com>
2024-04-24 16:06:46 -0400

    Multiview setup

--- milestone-02/src/js/app.js
+++ milestone-02/src/js/app.js
@@ -1,41 +1,61 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // sample playlist data
-    const playlists = [
-        { id: 1, name: 'Playlist 1', user: 'user101', genre: 'Hip-hop', votes: 25 },
-        { id: 2, name: 'Playlist 2', user: 'user102', genre: 'Rock', votes: 21 },
-    ];
+import * as mockdata from './mockdata.js';
 
-    function renderPlaylists() {
-        const container = document.getElementById('playlistContainer');
-        container.innerHTML = '';
+function showView(viewID) {
+    document.querySelectorAll(".view").forEach(function (view) {
+        view.style.display = "none";
+      });
+      // Show the requested view
+      document.getElementById(viewID).style.display = "block";
+}
 
-        playlists.forEach(playlist => {
-            const playlistElement = document.createElement('div');
-            playlistElement.classList.add('playlist');
-            // structure may change depending on how we want to use data with the back end
-            playlistElement.innerHTML = `
-                <h2>${playlist.name}</h2>
-                <p>by ${playlist.user}</p>
-                <p>${playlist.genre}</p>
-                <div>
-                    <span class="vote" onclick="vote(${playlist.id}, true)">👍</span>
-                    <span>${playlist.votes}</span>
-                    <span class="vote down" onclick="vote(${playlist.id}, false)">👎</span>
-                </div>
-            `;
-            container.appendChild(playlistElement);
-        });
-    }
-
-    // basic client side vote functionality
-    // will need to change to integrate with backend
-    window.vote = (playlistId, isUpvote) => {
-        const playlist = playlists.find(p => p.id === playlistId);
-        if (playlist) {
-            isUpvote ? playlist.votes++ : playlist.votes--;
-            renderPlaylists();
-        }
-    };
-
+document.getElementById("homePage").addEventListener("click", function () {
+    showView("homePage");
     renderPlaylists();
 });
+  
+document.getElementById("loginPage").addEventListener("click", function () {
+    showView("loginPage");
+});
+  
+document.getElementById("userProfilePage").addEventListener("click", function () {
+    showView("userProfilePage");
+});
+
+document.getElementById("playlistPage").addEventListener("click", function () {
+    showView("playlistPage");
+});
+
+function renderPlaylists() {
+    const container = document.getElementById('playlistContainer');
+    container.innerHTML = '';
+    mockdata.playlists.forEach(playlist => {
+        const playlistElement = document.createElement('div');
+        playlistElement.classList.add('playlist');
+        playlistElement.addEventListener('click', function() {
+            renderPlaylistPage(playlist.name);
+        });
+        // structure may change depending on how we want to use data with the back end
+        playlistElement.innerHTML = `
+            <h2>${playlist.name}</h2>
+            <p>by ${playlist.user}</p>
+            <p>${playlist.genre}</p>
+            <div>
+                <span class="vote" onclick="vote(${playlist.id}, true)">👍</span>
+                <span>${playlist.votes}</span>
+                <span class="vote down" onclick="vote(${playlist.id}, false)">👎</span>
+            </div>
+        `;
+        container.appendChild(playlistElement);
+    });
+}
+
+window.vote = (playlistId, isUpvote) => {
+    const playlist = mockdata.playlists.find(p => p.id === playlistId);
+    if (playlist) {
+        isUpvote ? playlist.votes++ : playlist.votes--;
+        renderPlaylists();
+    }
+};
+
+showView("homePage");
+renderPlaylists();
---
+++ milestone-02/src/js/mockdata.js
@@ -0,0 +1,44 @@
+export const playlists = [
+    { id: 1, name: 'Playlist 1', user: 'user101', genre: 'Hip-hop', votes: 25 },
+    { id: 2, name: 'Playlist 2', user: 'user102', genre: 'Rock', votes: 21 },
+];
+
+// songs
+export const song01 = {
+    title: "ULT",
+    artist: "Denzel Curry",
+    dateAdded: "April 23, 2024",
+    duration: "4:07",
+    genre: "Hip-hop"
+};
+export const song02 = {
+    title: "The Spins",
+    artist: "Mac Miller",
+    dateAdded: "April 23, 2024",
+    duration: "3:15",
+    genre: "Hip-hop"
+};
+export const song03 = {
+    title: "Higher",
+    artist: "Creed",
+    dateAdded: "April 23, 2024",
+    duration: "5:16",
+    genre: "Rock"
+};
+export const song04 = {
+    title: "Fade To Black (Remastered)",
+    artist: "Metallica",
+    dateAdded: "April 23, 2024",
+    duration: "6:57",
+    genre: "Rock"
+};
+
+// playlists
+export const playlist01 = [
+    song01,
+    song02
+];
+export const playlist02 = [
+    song03,
+    song04
+];
