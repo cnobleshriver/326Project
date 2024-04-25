5470a773f7a76c11ac68c0cf08f762312cb4a1d4
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2024-04-24 22:47:09 -0400

    add documentation

--- milestone-02/client/src/js/app.js
+++ milestone-02/client/src/js/app.js
@@ -1,28 +1,41 @@
 import * as mockdata from './mockdata.js';
-//import PouchDB from "pouchdb";
+// import PouchDB from "pouchdb";
 
-//const db = new PouchDB("profiles");
+// const db = new PouchDB("profiles");
+const loginBtn = document.getElementById("loginBtn");
+const submitBtn = document.getElementById("submit");
+const navbarLogo = document.getElementById("navbarLogo");
 
+/**
+ * Shows the view with the given ID by setting its display style to 'block',
+ * and hides all other views by setting their display style to 'none'.
+ * @param {string} viewID - The ID of the view to show.
+ */
 function showView(viewID) {
     document.querySelectorAll(".view").forEach(function (view) {
         view.style.display = "none";
-      });
-      // Show the requested view
-      document.getElementById(viewID).style.display = "block";
+    });
+    document.getElementById(viewID).style.display = "block";
 }
 
+/**
+ * Renders the playlist page for the given playlist name.
+ * @param {string} playlistName - The name of the playlist to render.
+ */
 function renderPlaylistPage(playlistName) {
     // render playlist page
     return;
 }
 
+/**
+ * Renders the home page by displaying all playlists in the mock data.
+ */
 function renderHomePage() {
     const container = document.getElementById('playlistContainer');
     container.innerHTML = '';
     mockdata.playlists.forEach(playlist => {
         const playlistElement = document.createElement('div');
         playlistElement.classList.add('playlist');
-        // structure may change depending on how we want to use data with the back end
         playlistElement.innerHTML = `
             <h2>${playlist.name}</h2>
             <p>by ${playlist.user}</p>
@@ -34,23 +47,35 @@ function renderHomePage() {
             </div>
         `;
         container.appendChild(playlistElement);
-        playlistElement.addEventListener('click', function() {
+        playlistElement.addEventListener('click', function () {
             showView("playlistPage");
             renderPlaylistPage(playlist.name);
         });
     });
 }
 
+/**
+ * Renders the login page.
+ */
 function renderLoginPage() {
     // TODO: render login page
     return;
 }
 
-//This function needs DB to function
+/**
+ * Logs in a user with the given username and password.
+ * @param {string} username - The username of the user to log in.
+ * @param {string} password - The password of the user to log in.
+ */
 async function login(username, password) {
     //await db.attemptLogin(username, password);
 }
 
+/**
+ * Votes for a playlist with the given ID.
+ * @param {string} playlistId - The ID of the playlist to vote for.
+ * @param {boolean} isUpvote - Whether the vote is an upvote or a downvote.
+ */
 window.vote = (playlistId, isUpvote) => {
     const playlist = mockdata.playlists.find(p => p.id === playlistId);
     if (playlist) {
@@ -59,27 +84,24 @@ window.vote = (playlistId, isUpvote) => {
     }
 };
 
-const loginBtn = document.getElementById("loginBtn");
-loginBtn.addEventListener("click", function() {
+// Event listeners
+loginBtn.addEventListener("click", function () {
     console.log("Login Screen Loading");
     showView("loginPage");
     //renderLoginPage();
 });
-
-const submitBtn = document.getElementById("submit")
-submitBtn.addEventListener("click", function() {
+submitBtn.addEventListener("click", function () {
     const usernameField = document.getElementById("username");
     const passwordField = document.getElementById("password");
     //login(usernameField.value, passwordField.value);
     showView("userProfilePage");
     //TODO: add renderUserProfile when said function is made
 });
-
-const appTitle = document.getElementById("appTitle");
-appTitle.addEventListener("click", function() {
+navbarLogo.addEventListener("click", function () {
     showView("homePage");
     renderHomePage();
 });
 
+// Initial view setup: show the home page and render it.
 showView("homePage");
 renderHomePage();
